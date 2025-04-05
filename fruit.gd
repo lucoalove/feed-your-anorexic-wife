43050636c6d57c6891c809607dc2800f7c58d1f9
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-04-05 15:07:37 -0400

    Fancy dragging fruit

--- fruit.gd
+++ fruit.gd
@@ -1,25 +1,29 @@
 extends Control
 
-# For making the cursor appear different on a control node: https://docs.godotengine.org/en/stable/classes/class_control.html#class-control-property-mouse-default-cursor-shape
+const DAMP := 5.0
 
-# handles movement with mause :O
-
-var is_hovered = false
-var is_held = false
+var is_hovered := false
+var is_held := false
+var velocity := Vector2(0, 0)
 
 func _ready() -> void:
+	
+	# https://docs.godotengine.org/en/stable/classes/class_control.html#class-control-property-mouse-default-cursor-shape
 	mouse_default_cursor_shape = Control.CursorShape.CURSOR_DRAG
 	
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	
+	position += velocity * delta
+	velocity = lerp(velocity, Vector2.ZERO, delta * DAMP)
 	
 	if is_held:
 		
 		var mouse_world_pos = get_viewport().get_mouse_position() * 1200 / get_viewport().get_visible_rect().size.x
 		
-		position = mouse_world_pos - Vector2(32, 32)
+		velocity += mouse_world_pos - (size / 2) - position
 
 func _on_mouse_entered():
 	
